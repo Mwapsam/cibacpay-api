e1312a731e71b0ba4313a97e49a2016217cda3de
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-19 16:49:23 +0200

    Implementation of login feature

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,13 @@
+class AuthenticationController < ApplicationController
+    skip_before_action :authenticate_request, only: [:login]
+
+    def login
+        @user = User.find_by_email(params[:email])
+        if @user&.authenticate(params[:password])
+            token = jwt_decode(user_id: @user.id)
+            render json: { token: token }, status: :ok
+        else
+            render json: { error: "Invalid username or password" }, status: :unauthorized
+        end
+    end
+end
