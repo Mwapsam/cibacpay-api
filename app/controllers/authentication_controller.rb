d57643b99f4763b5664466f4a69208bd98a12a9e
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-19 19:47:30 +0200

    Create user sessions

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,13 +1,26 @@
 class AuthenticationController < ApplicationController
-    skip_before_action :authenticate_request, only: [:login]
+  def create
+    user = User.find_by_email(authentication_params[:email])
 
-    def login
-        @user = User.find_by_email(params[:email])
-        if @user&.authenticate(params[:password])
-            token = jwt_decode(user_id: @user.id)
-            render json: { token: token }, status: :ok
-        else
-            render json: { error: "Invalid username or password" }, status: :unauthorized
-        end
+    if user&.authenticate(authentication_params[:password])
+      token = issue_token(user)
+      render json: { user: UserSerializer.new(user), jwt: token }
+    else
+      render json: { error: 'Incorrect username or password.' }
     end
+  end
+
+  def show
+    if logged_in?
+      render json: current_user
+    else
+      render json: { error: 'User is not logged in/could not be found.' }
+    end
+  end
+
+  private
+
+  def authentication_params
+    params.require(:authentication).permit(:email, :password)
+  end
 end
