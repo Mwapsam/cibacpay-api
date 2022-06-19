867b95ea4417d07f195ef02907152f6d5a9366ff
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-19 19:48:13 +0200

    Validate user properties

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-    require "securerandom"
+  require 'securerandom'
 
-    has_secure_password
+  has_secure_password
 
-    validates :email, presence: true, uniqueness: true, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
-    validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
+  validates :username, presence: true, uniqueness: true
 end
