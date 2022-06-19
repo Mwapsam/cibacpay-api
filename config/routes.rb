d9bac9e348d0befd0f5565185ace3eefea81f468
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-19 19:48:25 +0200

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json } do
-    namespace :v1 do
-      get 'hello', to: 'hello#index'
-    end
-  end
+  resources :users, only: [:create, :show, :update]
+    post "/signup", to: "users#create"
+    post "/login", to: "authentication#create"
+    get "/authorized", to: "authentication#show"
+  # namespace :api, defaults: { format: :json } do
+  #   namespace :v1 do
+  #     get 'hello', to: 'hello#index'
+  #   end
+  # end
 end
