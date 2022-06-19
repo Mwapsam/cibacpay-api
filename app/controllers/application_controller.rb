eab09e63a96fac105172d6e4cce7fdf40e55fbba
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-19 19:46:52 +0200

    Encrypt user data using jwt_token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,31 @@
 class ApplicationController < ActionController::API
-    include JsonWebToken
+  def jwt_key
+    Rails.application.secrets.secret_key_base
+  end
 
-    before_action :authenticate_request
+  def issue_token(user)
+    JWT.encode({ user_id: user.id }, jwt_key, 'HS256')
+  end
 
-    private
+  def decoded_token
+    JWT.decode(token, jwt_key, true, { algorithm: 'HS256' })
+  rescue StandardError
+    [{ error: 'Invalid Token' }]
+  end
 
-    def authenticate_request
-        header = request.headers["Authorization"]
-        header = header.split(" ").last if header
-        decoded = jwt_decode(header)
-        @current_user = User.find(decoded["user_id"])
-    end
+  def token
+    request.headers['Authorization']
+  end
+
+  def user_id
+    decoded_token.first['user_id']
+  end
+
+  def current_user
+    User.find_by(id: user_id)
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
