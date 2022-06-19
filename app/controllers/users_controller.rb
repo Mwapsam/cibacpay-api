15d588bb3d043a7f733b99802128f72d063296b4
Samuel Mwape <43408618+Mwapsam@users.noreply.github.com>
2022-06-19 16:33:45 +0200

    Created user actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+    skip_before_action :authenticate_request, only: [:create]
+    before_action :set_user, only: [:show, :destroy]
+
+    def index
+        @users = User.all
+        render json: @users, status: :ok
+    end
+
+    def show
+        render json: @users, status: :ok
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            render json: @user, status: :created
+        else
+            render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @user.destroy
+    end
+
+    def update
+        unless @user.update(user_params)
+            render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :email, :password)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+end
